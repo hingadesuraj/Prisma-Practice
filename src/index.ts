8c1319591b8b844e14b5a9150183db7e62496b99
Suraj <hingadesuraj@gmail.com>
2024-02-25 12:47:34 +0530

    insert data in database using prisma impliment

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,15 @@ const prisma = new PrismaClient();
 
 
 
+async function insertUser(username:string,firstName:string,lastName:string,password:string){
+    const res  = await prisma.user.create({
+        data:{
+            email:username,
+            firstName,
+            lastName,
+            password
+        }
+    })
+    console.log(res)
+}
+
