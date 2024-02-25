8f1c19bf1307c05153c33c85f364919d46c50a8e
Suraj <hingadesuraj@gmail.com>
2024-02-25 13:27:19 +0530

    update data using prisma impliment

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,56 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+// insert Data in DB
 
+async function insertUser(
+  username: string,
+  firstName: string,
+  lastName: string,
+  password: string
+) {
+  const res = await prisma.user.create({
+    data: {
+      email: username,
+      firstName,
+      lastName,
+      password,
+    },
+    select: {
+      id: true,
+      email: true,
+    },
+  });
+  console.log(res);
+}
 
-async function insertUser(username:string,firstName:string,lastName:string,password:string){
-    const res  = await prisma.user.create({
-        data:{
-            email:username,
+// insertUser('suraj@gmail.com','suraj','go','password');
+
+// Update Data in DB
+
+// interface in typescript 
+interface UpdateParams {
+  firstName: string;
+  lastName: string;
+}
+
+async function updateUser(
+  username: string,
+  { firstName, lastName }: UpdateParams
+) {
+    const res = await prisma.user.update({
+        where: { email : username}, // where can you update data 
+        data : {
             firstName,
-            lastName,
-            password
+            lastName
         },
         select:{
-            id:true,
-            email:true
+            firstName:true
         }
+
     })
     console.log(res)
 }
 
-insertUser('suraj@gmail.com','suraj','go','password');
+
+// updateUser('suraj@gmail.com',{firstName:"surajbhau",lastName:"hingadebhau"});
